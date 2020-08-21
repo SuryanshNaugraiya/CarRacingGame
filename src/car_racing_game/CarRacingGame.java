2d358d555ba1bfe9f723050717be4dfa0afef7a5
SuryanshNaugraiya <suryansh.naugraiya_cs18@gla.ac.in>
2020-08-21 15:51:43 +0530

    draw 3 rectangles and set its color white

--- src/car_racing_game/CarRacingGame.java
+++ src/car_racing_game/CarRacingGame.java
@@ -7,13 +7,19 @@ public class CarRacingGame extends Applet {
 
     @Override
     public void start() {
+
         setSize(800,600);
         setBackground(Color.green);
     }
 
     @Override
     public void paint(Graphics g) {
+
         g.setColor(Color.black);
         g.fillRect(200,0,400,600);
+        g.setColor(Color.white);
+        g.fillRect(380,40,40,150);
+        g.fillRect(380,230,40,150);
+        g.fillRect(380,420,40,150);
     }
 }
